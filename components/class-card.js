905d40b074a73cb08f7ed5984ab298b38d515955
Matias Crivolotti <matioav@gmail.com>
2021-01-21 18:30:26 +0200

    class card component

---
+++ components/class-card.js
@@ -0,0 +1,82 @@
+import {
+  AnchorButton,
+  Button,
+  Card,
+  Code,
+  Divider,
+  H3,
+  H4,
+  Intent,
+  Position,
+  Tooltip,
+} from "@blueprintjs/core";
+
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Fragment } from "react";
+import dayjs from "dayjs";
+import dynamic from "next/dynamic";
+
+const Contents = dynamic(() => import("./contents"), { ssr: false });
+
+const ClassCard = ({ title, contents, zooms }) => {
+  const onCopy = (message, result) => {
+    if (result) {
+      AppToasterSuccess.show({ message: "הועתק בהצלחה" });
+    } else {
+      AppToasterFail.show({ message: "העתקה נכשלה" });
+    }
+  };
+
+  return (
+    <Card>
+      <H3>{title}</H3>
+      {contents ? <Contents contents={contents} /> : null}
+      {zooms.length > 0 ? (
+        <>
+          {zooms.map((zoom, zdx) => (
+            <Fragment key={`zoom-${zdx}`}>
+              <div>
+                <H4>פגישת זום:</H4>
+                <p>בשעה: {dayjs(zoom.time).format("HH:mm")}</p>
+                {zoom.contents ? <Contents contents={zoom.contents} /> : null}
+                <AnchorButton
+                  outlined
+                  intent={Intent.PRIMARY}
+                  href={zoom.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  לחצו כאן
+                </AnchorButton>
+                {zoom?.meetingId ? (
+                  <p>
+                    <strong>ID ישיבה:</strong> <Code>{zoom.meetingId}</Code>{" "}
+                    <Tooltip content="העתק" position={Position.TOP}>
+                      <CopyToClipboard text={zoom.meetingId} onCopy={onCopy}>
+                        <Button icon="duplicate" small minimal />
+                      </CopyToClipboard>
+                    </Tooltip>
+                    <br />
+                    {zoom.meetingPassword ? (
+                      <>
+                        <strong>סיסמה:</strong> <Code>{zoom.meetingPassword}</Code>{" "}
+                        <Tooltip content="העתק" position={Position.TOP}>
+                          <CopyToClipboard text={zoom.meetingPassword} onCopy={onCopy}>
+                            <Button icon="duplicate" small minimal />
+                          </CopyToClipboard>
+                        </Tooltip>
+                      </>
+                    ) : null}
+                  </p>
+                ) : null}
+              </div>
+              {zdx < zooms.length - 1 ? <Divider /> : null}
+            </Fragment>
+          ))}
+        </>
+      ) : null}
+    </Card>
+  );
+};
+
+export default ClassCard;
--- components/preview.js
+++ components/preview.js
@@ -1,24 +1,6 @@
-import {
-  AnchorButton,
-  Button,
-  Card,
-  Code,
-  Divider,
-  H3,
-  H4,
-  H5,
-  Intent,
-  Position,
-  Tooltip,
-} from "@blueprintjs/core";
-
-import { CopyToClipboard } from "react-copy-to-clipboard";
-import { Fragment } from "react";
-import dayjs from "dayjs";
-import dynamic from "next/dynamic";
+import ClassCard from "./class-card";
+import { H5 } from "@blueprintjs/core";
 import { useWatch } from "react-hook-form";
-
-const Contents = dynamic(() => import("./contents"), { ssr: false });
 
 const Preview = ({ control }) => {
   const title = useWatch({
@@ -37,65 +19,10 @@ const Preview = ({ control }) => {
     defaultValue: [],
   });
 
-  const onCopy = (message, result) => {
-    if (result) {
-      AppToasterSuccess.show({ message: "הועתק בהצלחה" });
-    } else {
-      AppToasterFail.show({ message: "העתקה נכשלה" });
-    }
-  };
-
   return (
     <div>
       <H5>תצוגה מקדימה</H5>
-      <Card>
-        <H3>{title}</H3>
-        {contents ? <Contents contents={contents} /> : null}
-        {zooms.length > 0 ? (
-          <>
-            {zooms.map((zoom, zdx) => (
-              <Fragment key={`zoom-${zdx}`}>
-                <div>
-                  <H4>פגישת זום:</H4>
-                  <p>בשעה: {dayjs(zoom.time).format("HH:mm")}</p>
-                  {zoom.contents ? <Contents contents={zoom.contents} /> : null}
-                  <AnchorButton
-                    outlined
-                    intent={Intent.PRIMARY}
-                    href={zoom.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    לחצו כאן
-                  </AnchorButton>
-                  {zoom?.meetingId ? (
-                    <p>
-                      <strong>ID ישיבה:</strong> <Code>{zoom.meetingId}</Code>{" "}
-                      <Tooltip content="העתק" position={Position.TOP}>
-                        <CopyToClipboard text={zoom.meetingId} onCopy={onCopy}>
-                          <Button icon="duplicate" small minimal />
-                        </CopyToClipboard>
-                      </Tooltip>
-                      <br />
-                      {zoom.meetingPassword ? (
-                        <>
-                          <strong>סיסמה:</strong> <Code>{zoom.meetingPassword}</Code>{" "}
-                          <Tooltip content="העתק" position={Position.TOP}>
-                            <CopyToClipboard text={zoom.meetingPassword} onCopy={onCopy}>
-                              <Button icon="duplicate" small minimal />
-                            </CopyToClipboard>
-                          </Tooltip>
-                        </>
-                      ) : null}
-                    </p>
-                  ) : null}
-                </div>
-                {zdx < zooms.length - 1 ? <Divider /> : null}
-              </Fragment>
-            ))}
-          </>
-        ) : null}
-      </Card>
+      <ClassCard title={title} contents={contents} zooms={zooms} />
     </div>
   );
 };
